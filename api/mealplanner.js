82292a8c3f201458e1dc2903d1ce2db725f2d9df
cokerj03 <cokerj03@gmail.com>
2025-02-26 10:10:10 -0500

    new api key

--- api/mealplanner.js
+++ api/mealplanner.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     
     try {
       // Construct the request URL using the provided API key
-      const edamamResponse = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(query)}&app_id=70a6ee50&app_key=187e5d6682376ce9ff06148fe538e51c`);
+      const edamamResponse = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${encodeURIComponent(query)}&app_id=a0d62e98&app_key=bb67a30b4ec2d751721fdf6b8f59a2c2`);
 
 
       
